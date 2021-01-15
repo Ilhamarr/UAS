15ab7d9aec4fd0bba402599a4700d669b8d315d7
Ilhamarr <ilhamarr40@gmail.com>
2021-01-15 15:16:11 +0700

    uas

--- app/src/main/java/com/example/uas/MainActivity.java
+++ app/src/main/java/com/example/uas/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.uas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView output;
+    EditText url;
+    Button Mainbutton;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        url = findViewById(R.id.url);
+        Mainbutton = findViewById(R.id.mainbutton);
+        spinner = findViewById(R.id.spin);
+
     }
 }
